abd3633310dd1c74301838e681c5ad3c3ae8092c
Edgar Munoz <edgar.munoz88@hotmail.com>
2024-11-10 09:57:35 -0500

    Initial Commit

---
+++ RecepiesSwiftUI/RecepiesSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecepiesSwiftUIApp.swift
+//  RecepiesSwiftUI
+//
+//  Created by Munoz, Edgar on 11/10/24.
+//
+
+import SwiftUI
+
+@main
+struct RecepiesSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RecepiesSwiftUITests/RecepiesSwiftUITests.swift
@@ -0,0 +1,17 @@
+//
+//  RecepiesSwiftUITests.swift
+//  RecepiesSwiftUITests
+//
+//  Created by Munoz, Edgar on 11/10/24.
+//
+
+import Testing
+@testable import RecepiesSwiftUI
+
+struct RecepiesSwiftUITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
